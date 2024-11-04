aa7a15627533b751e7795f79022d9e8b5d937b85
Tboules <tboules@gmail.com>
2024-11-04 12:19:47 -0800

    zsh updates

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,13 +2,15 @@ local wezterm = require("wezterm")
 
 local config = {}
 
+config.color_scheme = "Classic Dark (base16)"
+-- config.color_scheme = "GitHub Dark"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 13.0
+config.font_size = 15.0
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 50
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 20
 
 return config
