a5e7a6b70536fc154ddf92a859b742346a2dd3ce
Tboules <tboules@gmail.com>
2026-06-18 09:40:41 -0700

    wezterm fontchange

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,13 +5,13 @@ local config = {}
 config.color_scheme = "Classic Dark (base16)"
 -- config.color_scheme = "GitHub Dark"
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 20.0
+config.font_size = 17.0
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
--- config.window_background_opacity = 0.95
--- config.macos_window_background_blur = 20
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 20
 
 wezterm.on("user-var-changed", function(window, pane, name, value)
 	local overrides = window:get_config_overrides() or {}
